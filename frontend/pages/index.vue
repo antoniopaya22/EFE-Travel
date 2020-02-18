bc6e082c89d94db40b262c1b77d35b1d6f8382fa
antonioalfa22 <antonioalfa22@gmail.com>
2020-02-18 09:14:54 +0100

    Separada funcionalidad en componentes

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -103,97 +103,36 @@
     <!-- FILA 1 -->
     <v-row align="center" justify="center">
       <v-col cols="12">
-        <v-card class="mx-auto" color="#363636">
-          <v-img
-            class="white--text align-end"
-            height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          >
-            <v-card-title>{{viajesDestacado.title}}</v-card-title>
-          </v-img>
-
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="orange" text>Share</v-btn>
-
-            <v-btn color="orange" text>Explore</v-btn>
-          </v-card-actions>
-        </v-card>
+        <Recomendacion :recomendacion="viajesDestacado" />
       </v-col>
     </v-row>
 
     <!-- FILA 2 -->
     <v-row>
       <v-col cols="12" lg="6" md="6" sm="12" xs="12" v-for="viaje in viajes2" :key="viaje.id">
-        <v-card class="mx-auto" color="#363636">
-          <v-img
-            class="white--text align-end"
-            height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          >
-            <v-card-title>{{viaje.title}}</v-card-title>
-          </v-img>
-
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="orange" text>Share</v-btn>
-
-            <v-btn color="orange" text>Explore</v-btn>
-          </v-card-actions>
-        </v-card>
+        <Recomendacion :recomendacion="viaje" />
       </v-col>
     </v-row>
 
     <!-- FILA 3 -->
     <v-row>
       <v-col cols="12" lg="3" md="3" sm="6" xs="12" v-for="viaje in viajes3" :key="viaje.id">
-        <v-card class="mx-auto" color="#363636">
-          <v-img
-            class="white--text align-end"
-            height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          >
-            <v-card-title>{{viaje.title}}</v-card-title>
-          </v-img>
-
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="orange" text>Share</v-btn>
-
-            <v-btn color="orange" text>Explore</v-btn>
-          </v-card-actions>
-        </v-card>
+        <Recomendacion :recomendacion="viaje" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import Recomendacion from '~/components/Recomendacion'
+
 const Cookie = process.client ? require("js-cookie") : undefined;
 
 export default {
   layout: "default",
+  components: {
+    Recomendacion
+  },
   data: () => ({
     searchOrigen: null,
     selectOrigen: null, //Elemento seleccionado origen
--- frontend/pages/vuelos/buscar.vue
+++ frontend/pages/vuelos/buscar.vue
@@ -50,29 +50,7 @@
     <!-- FILA RESULTADOS -->
     <v-row align="center" justify="center" v-for="vuelo in vuelos" :key="vuelo.id">
       <v-col cols="12">
-        <v-card class="mx-auto">
-          <v-img
-            class="white--text align-end"
-            height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          >
-            <v-card-title>{{vuelo.title}}</v-card-title>
-          </v-img>
-
-          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-          <v-card-text class="text--primary">
-            <div>Whitehaven Beach</div>
-
-            <div>Whitsunday Island, Whitsunday Islands</div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="orange" text>Share</v-btn>
-
-            <v-btn color="orange" text>Explore</v-btn>
-          </v-card-actions>
-        </v-card>
+          <Vuelo :vuelo="vuelo" />
       </v-col>
     </v-row>
   </v-container>
@@ -86,10 +64,15 @@
 </style>
 
 <script>
+import Vuelo from '~/components/Vuelo';
+
 const Cookie = process.client ? require("js-cookie") : undefined;
 
 export default {
   layout: "default",
+  components: {
+    Vuelo
+  },
   data: () => ({
     destino: null,
     entrada: null,
