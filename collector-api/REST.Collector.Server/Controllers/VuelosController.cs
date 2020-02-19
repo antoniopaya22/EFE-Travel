d9a77a72228fd9ba4b99e7308668e7e2a31d1d76
antonioalfa22 <antonioalfa22@gmail.com>
2020-02-19 15:57:10 +0100

    Añadido verify token

---
+++ collector-api/REST.Collector.Server/Controllers/VuelosController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using REST.Collector.Server.Model;
+using RestSharp;
+
+namespace REST.Collector.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VuelosController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public static IList<Vuelo> database = new List<Vuelo>(new[]
+        {
+            new Vuelo { 
+                Origen = "Madrid",
+                Destino = "Barcelona",
+                FechaSalidaIda = new DateTime(),
+                FechaLlegadaIda = new DateTime(),
+                Personas = 2,
+                Precio = 22.5,
+                Aerolinea = "Ryanair",
+                Enlace = ""
+            },
+            new Vuelo {
+                Origen = "Madrid",
+                Destino = "Asturias",
+                FechaSalidaIda = new DateTime(),
+                FechaLlegadaIda = new DateTime(),
+                Personas = 2,
+                Precio = 82.5,
+                Aerolinea = "Vueling",
+                Enlace = ""
+            },
+            new Vuelo {
+                Origen = "Madrid",
+                Destino = "Venecia",
+                FechaSalidaIda = new DateTime(),
+                FechaLlegadaIda = new DateTime(),
+                Personas = 2,
+                Precio = 132.0,
+                Aerolinea = "AirEuropa",
+                Enlace = ""
+            }
+        });
+
+        public VuelosController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        private bool validateToken(string token)
+        {
+            var client = new RestClient(_configuration.GetValue<string>("ApplicationSettings:TokensEndPoint"));
+            var getRequest = new RestRequest("/verify-token", Method.GET);
+            getRequest.AddHeader("Authorization", token);
+            var response = client.Execute(getRequest);
+            if(response.IsSuccessful)
+                return true;
+            return false;
+        }
+
+        [HttpGet]
+        public ActionResult Get([FromHeader] string authorization)
+        {
+            if (String.IsNullOrEmpty(authorization) || !validateToken(authorization))
+                return Unauthorized("Token invalido");
+            return Ok(database);
+        }
+
+
+    }
+}
---
+++ collector-api/REST.Collector.Server/Model/Vuelo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.Collector.Server.Model
+{
+    public class Vuelo
+    {
+        public string Origen { get; set; }
+        public string Destino { get; set; }
+        public DateTime FechaSalidaIda { get; set; }
+        public DateTime FechaLlegadaIda { get; set; }
+        public DateTime FechaSalidaVuelta { get; set; }
+        public DateTime FechaLlegadaVuelta { get; set; }
+        public int Personas { get; set; }
+        public double Precio { get; set; }
+        public string Aerolinea { get; set; }
+        public string Enlace { get; set; }
+    }
+}
