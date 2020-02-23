e6b645ef09d8f37ad46daa96979b12dd789f2173
albact7 <alba164@gmail.com>
2020-02-23 19:21:30 +0100

    Arreglado SOAP server + token propio

--- collector-api/SOAP.Collector.Server/SoapCollector.asmx.cs
+++ collector-api/SOAP.Collector.Server/SoapCollector.asmx.cs
@@ -1,10 +1,12 @@
-﻿using SOAP.Collector.Server.Adapters;
+﻿using RestSharp;
+using SOAP.Collector.Server.Adapters;
 using SOAP.Collector.Server.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Web.Services.Protocols;
 
 namespace SOAP.Collector.Server
 {
@@ -14,12 +16,29 @@ namespace SOAP.Collector.Server
     [System.ComponentModel.ToolboxItem(false)]
     public class SoapCollector : WebService
     {
+        public AuthorizationHeader authorization;
+        private bool validateToken(string token)
+        {
+            var client = new RestClient("http://localhost:5000");
+            var getRequest = new RestRequest("/verify-token", Method.GET);
+            getRequest.AddHeader("Authorization", token);
+            var response = client.Execute(getRequest);
+            return response.IsSuccessful;
+        }
 
         [WebMethod]
+        [SoapHeader("authorization", Direction = SoapHeaderDirection.In)]
         public List<Recommendation> GetRecomendaciones(string origin)
         {
+            if (String.IsNullOrEmpty(authorization.token) || !validateToken(authorization.token))
+                throw new SoapException("Token inválido", SoapException.ClientFaultCode);
             IRecommendationsCollector recCollector = new AmadeusRecommendationAdapter();
             return recCollector.GetRecommendations(origin);
+        }
+
+        public class AuthorizationHeader : SoapHeader
+        {
+            public string token;
         }
     }
 }
