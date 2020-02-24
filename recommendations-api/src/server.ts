95d8693ab81b786adebec55f0df3ad33ecbf92ee
antonioalfa22 <antonioalfa22@gmail.com>
2020-02-24 09:00:28 +0100

    Cambiados hosts

--- recommendations-api/src/server.ts
+++ recommendations-api/src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-const PORT = process.env.PORT || 8070;
+const PORT = Number.parseInt(process.env.PORT) || 8070;
 
 app.listen(PORT, '0.0.0.0',  () => {
     console.log(`API REST corriendo en puerto ${PORT}`);
