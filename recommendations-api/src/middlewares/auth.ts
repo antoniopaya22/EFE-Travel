578c11d088fbdb7f9463212801951c91d88d9b6e
antonioalfa22 <antonioalfa22@gmail.com>
2020-02-24 08:53:53 +0100

    Cambiados hosts

--- recommendations-api/src/middlewares/auth.ts
+++ recommendations-api/src/middlewares/auth.ts
@@ -4,7 +4,7 @@ import * as rp from 'request-promise';
 export class Auth {
 
     public isAuth(req: Request, res: Response, next) {
-        const tokensapi = process.env.TOKENS_API || 'http://localhost:5000';
+        const tokensapi = process.env.TOKENS_API || 'http://156.35.95.126:5000';
         rp(
             {
                 uri: tokensapi + '/verify-token',
